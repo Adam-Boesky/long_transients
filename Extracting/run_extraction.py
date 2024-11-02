8a823060486cbb7d6e3ada06f7b1344487e7c45e
Adam-Boesky <apboesky@gmail.com>
2024-11-02 12:36:34 -0400

    Update some stuff on the cluster side

--- Extracting/run_extraction.py
+++ Extracting/run_extraction.py
@@ -25,11 +25,11 @@ def process_quadrant(quadrant, field_id, data_path, parallel):
         data_dir=os.path.join(data_path, 'ztf_data'),
         parallel=parallel
     )
-    tile_output_path = tile.store_catalogs(os.path.join(data_path, 'local_catalog_results'), overwrite=True)
+    tile_output_path = tile.store_catalogs(os.path.join(data_path, 'catalog_results'), overwrite=True)
 
-    # For testing, store the tile objects as well (NOTE: DELTE THIS IN THE FUTURE)
-    with open(os.path.join(tile_output_path, 'tile.pkl'), 'wb') as f:
-        pickle.dump(tile, f)
+    # # For testing, store the tile objects as well (NOTE: DELTE THIS IN THE FUTURE)
+    # with open(os.path.join(tile_output_path, 'tile.pkl'), 'wb') as f:
+    #     pickle.dump(tile, f)
 
 
 def process_field(field_id, field_poly, data_path, parallel):
--- Extracting/run_extraction.sh
+++ Extracting/run_extraction.sh
@@ -3,11 +3,11 @@
 #SBATCH -p test
 #SBATCH -c 12                                                                                           # Number of cores (-c)
 #SBATCH --mem=128G                                                                                      # Memory pool for all cores (see also --mem-per-cpu)
-#SBATCH -t 0-01:30                                                                                      # Runtime in D-HH:MM, minimum of 10 minutes
+#SBATCH -t 0-03:00                                                                                      # Runtime in D-HH:MM, minimum of 10 minutes
 
 # Paths to STDOUT or STDERR files should be absolute or relative to current working directory
-#SBATCH -o /n/home04/aboesky/berger/long_transients/Extracting/training_logs/myoutput_\%j.out           # File to which STDOUT will be written, %j inserts jobid
-#SBATCH -e /n/home04/aboesky/berger/long_transients/Extracting/training_logs/myerrors_\%j.err           # File to which STDERR will be written, %j inserts jobid
+#SBATCH -o /n/home04/aboesky/berger/long_transients/Extracting/extraction_logs/myoutput_\%j.out           # File to which STDOUT will be written, %j inserts jobid
+#SBATCH -e /n/home04/aboesky/berger/long_transients/Extracting/extraction_logs/myerrors_\%j.err           # File to which STDERR will be written, %j inserts jobid
 #SBATCH --mail-user=aboesky@college.harvard.edu     # Send email to user
 
 # Remember:
