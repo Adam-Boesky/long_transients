Fix masking stuff in PSTARR catalog.

Extracting/Catalogs.py
@@ -40,6 +40,11 @@ class Catalog():
             self.rename_columns()
         return self._data
 
+    @data.setter
+    def data(self, table: Table):
+        self._data = table
+        self.rename_columns()
+
     def get_data(self) -> Table:
         raise NotImplementedError('This method must be implemented in subclass.')
 
@@ -224,7 +229,15 @@ WHERE rn = 1
         for tab in tabs:
             if final_table is None:
                 final_table = tab
-            final_table = join(final_table, tab, join_type='outer')
+
+            if len(final_table) == 0:
+                final_table = tab
+            elif len(tab) != 0:
+                final_table = join(final_table, tab, join_type='outer')
+            elif len(tab) == 0:
+                for col in tab.colnames:
+                    if col not in final_table.colnames:
+                        final_table[col] = np.zeros(len(final_table)) * np.nan
 
         return final_table
 
@@ -280,6 +293,22 @@ WHERE rn = 1
 
         # Drop residual row number column
         if 'rn' in final_table.columns: final_table.remove_column('rn')
+
+        # Fill in mask with nans
+        final_table = Table(final_table, masked=True)
+        for col in final_table.colnames:
+            column = final_table[col]
+            if np.issubdtype(column.dtype, np.number):
+                # Convert column to float if numeric to allow np.nan
+                final_table[col] = column.astype(float)
+                final_table[col][column.mask] = np.nan
+            else:
+                # Convert to object dtype for non-numeric columns
+                final_table[col] = column.astype(object)
+                final_table[col][column.mask] = np.nan
+
+        # Unmask the table (removes the mask attribute entirely)
+        final_table = Table(final_table, masked=False)
 
         return final_table
 

Extracting/cross_match.py
@@ -71,7 +71,7 @@ def associate_tables(table1: Table, table2: Table, ztf_nan_mask: np.ndarray, wcs
     unmatched_table1 = table1[unmatched_idx1]
     unmatched_table2 = table2[unmatched_idx2]
 
-    # Add columns from table2 to unmatched_table1 with NaN values and stack the tables
+    # Add columns from table2 to unmatched_table1 with NaN values, cast cols to correct types, and stack the tables
     for col in table2.colnames:
         unmatched_table1[col] = float('nan')
     for col in table1.colnames:
@@ -79,6 +79,11 @@ def associate_tables(table1: Table, table2: Table, ztf_nan_mask: np.ndarray, wcs
     associated_table['Catalog'] = 'Both'
     unmatched_table1['Catalog'] = prefix1
     unmatched_table2['Catalog'] = prefix2
+
+    # Cast PSTARR_primaryDetection to float64 in associated_table, unmatched_table1, unmatched_table2
+    for table in [associated_table, unmatched_table1, unmatched_table2]:
+        if 'PSTARR_primaryDetection' in table.colnames:
+            table['PSTARR_primaryDetection'] = table['PSTARR_primaryDetection'].astype(np.float64)
     combined_table = vstack([associated_table, unmatched_table1, unmatched_table2])
 
     # Identify rows where 'association_separation_arcsec' should be NaN