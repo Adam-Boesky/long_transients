f24bf28f89d8e8146584bb95465ff728cc255446
Adam Boesky <aboesky@college.harvard.edu>
2025-07-21 19:41:34 -0600

    Add extraction info script

---
+++ Extracting/store_extraction_info.py
@@ -0,0 +1,21 @@
+import os
+import json
+
+from utils import get_n_quadrants_merged_all_fields, get_data_path
+
+
+def store_extraction_info():
+    """Store the extraction info for each candidate."""
+    n_quadrants_merged = get_n_quadrants_merged_all_fields()
+    data_path = get_data_path()
+
+    # Prepare the output directory and file path
+    output_file = os.path.join(data_path, 'catalog_results/extraction_info.json')
+
+    # Store n_quadrants_merged in the JSON file
+    with open(output_file, 'w') as f:
+        json.dump(n_quadrants_merged, f)
+
+
+if __name__ == '__main__':
+    store_extraction_info()
