bfb4164bfd789a7b9ca52c1c2cbb65f0c7845927
Adam Boesky <aboesky@college.harvard.edu>
2025-11-17 23:25:20 -0500

    adding kde calc script

---
+++ Source_Analysis/calculate_kdes.py
@@ -0,0 +1,36 @@
+import sys
+import os
+import pickle
+import ztffields
+import numpy as np
+
+from ztffields.fields import get_fieldid
+
+sys.path.append('/Users/adamboesky/Research/long_transients')
+
+from Source_Analysis.utils import get_kde, get_data_path
+
+
+def calculate_kdes():
+
+    # Get the fields that we want KDEs for
+    imaged_fields = {}
+    for band in ('g', 'r', 'i'):
+        imaged_fields[band] = np.load(f'/Users/adamboesky/Research/long_transients/Data/{band}_imaged_fields.npy')
+    fs = np.intersect1d(get_fieldid(grid='main', dec_range=[-100, -5], ra_range=[75, 100]), imaged_fields['g'])
+    print(f'Calculating KDEs for fields (n = {len(fs)}): {fs}')
+
+    # Calculate the KDEs
+    kdes = {}
+    for band in ('g', 'r', 'i'):
+        kdes[band] = get_kde(fs, band)
+
+    # Save the KDEs
+    data_path = get_data_path()
+    for band, kde in kdes.items():
+        with open(os.path.join(data_path, f'{band}_kde.pkl'), 'wb') as f:
+            pickle.dump(kde, f)
+
+
+if __name__ == '__main__':
+    calculate_kdes()
